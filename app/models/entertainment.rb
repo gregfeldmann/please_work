f8c516e8d575715edc3e167e590bfe48b24e08a6
Raghu Betina <raghu@firstdraft.com>
2017-08-21 21:26:06 +0000

    Direct association between entertainments and recommends

--- app/models/entertainment.rb
+++ app/models/entertainment.rb
@@ -1,6 +1,9 @@
 class Entertainment < ApplicationRecord
   # Direct associations
 
+  has_many   :recommends,
+             :dependent => :destroy
+
   has_many   :ratings,
              :dependent => :destroy
 
